4fe35d93ac8dc5bde3619eef76ea79148e636441
pc <tsikavii.chelovek@gmail.com>
2024-08-21 14:09:06 +0300

    Add file3

--- Lab3.py
+++ Lab3.py
@@ -1,6 +1,13 @@
-n = 5
+n = int(input("Будь ласка, введіть ціле число (1 < N < 9): "))
+line = ""
+while n < 1 or n > 9:
+    line += "!"
+    n = int(input(f"Будь ласка, введіть ціле число (1 < N < 9 {line}): "))
+
 for i in range(1, n + 1):
-    line = ""
-    for j in range(n - i + 1, n + 1):
-        line += str(j)
-    print(line)
+    print("  " * (n - i), end="")
+
+    for j in range(n, n - i, -1):
+        print(j, end=" ")
+
+    print()
